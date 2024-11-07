b23c1b424c3b00e7878683cd968695cd3420eb84
kkwryt <ayxanquseynov202@gmail.com>
2024-11-08 01:18:22 +0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+// Функции JavaScript, если необходимо
+$(document).ready(function() {
+  // Пример с кнопкой
+  $("button").click(function() {
+    alert("Супер! Вы только что подписались!");
+  });
+});
